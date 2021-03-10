674fdc4adb9fbff90729667767ae49e66deeab7a
Ahsan Khan <ahsanszic@gmail.com>
2021-03-10 20:48:20 +0500

    database

--- db/conn.php
+++ db/conn.php
@@ -5,10 +5,10 @@ $password = "";
 $dbname="technagers_db";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=technagers_db", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=technager_db", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//   echo "Connected successfully";
+  echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
