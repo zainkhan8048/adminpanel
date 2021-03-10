a818f2e0c28328c12f8064cbf3e71a92459a5f6e
Zain Khan <zainkhan8048@gmail.com>
2021-03-10 20:40:57 +0500

    Update index.php

--- demo1/index.php
+++ demo1/index.php
@@ -5,11 +5,7 @@
 <head>
 		
 		<meta charset="utf-8" />
-		<title>Live preview | Technagers
-		
-		
-		
-		</title>
+		<title>Live preview | Technagers</title>
 		<meta name="description" content="In house System." />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		
