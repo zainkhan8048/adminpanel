50aeb5c752209499363bf5068f40776b5ccd8a1a
Asees Ahmed <aseesahmed@gmail.com>
2021-03-10 21:40:38 +0500

    Update login.php

--- demo1/custom/pages/login/login-4/login.php
+++ demo1/custom/pages/login/login-4/login.php
@@ -54,84 +54,16 @@
                             <!--begin::Title-->
                             <div class="pb-5 pb-lg-15">
 							<?php
-								include '../../../../../db/conn.php';
 								
-					if(isset($_POST['login']))
-					{
-						
-						//Retrieve the field values from our login form.
-						$user_email = !empty($_POST['user_email']) ? trim($_POST['user_email']) : null;
-						$passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
-
-						//Retrieve the subscriber account information for the given user_email.
-						$sql = "SELECT u.`user_id`, u.`user_first_name`, u.`user_last_name`, u.`user_email`, u.`user_phone`, u.`user_password`, u.`user_login_time`, u.`user_logout_time`, r.`role_id` ,r.role_type
-								FROM `tbl_user` u, tbl_role r
-								WHERE u.role_id = r.role_id
-								AND u.user_email = :user_email";
-						//echo $sql;
-						//die();
-						$stmt = $conn->prepare($sql);
-						$stmt->bindValue(':user_email', $user_email);
-						$stmt->execute();
-						$row = $stmt->fetch(PDO::FETCH_ASSOC);
-						
-						//Fetch db row username.
-						$dbuser_email = $row['user_email'];
-
-						//If $row is FALSE.
-						if($dbuser_email !== $user_email)
-						{
-							//Could not find a subscriber with that username!
-							//header("Location: /msgs/msg_invalid_username.php");
-							echo "invalid_username";
-							
-						}
-						else
-						{
-							
-							//subscriber account found. Check to see if the given password matches the
-							//password hash that we stored in our subscriber table.
-							
-							//Compare the passwords.
-							$validPassword = password_verify($passwordAttempt, $row['user_password']);
-							
-
-							//If $validPassword is TRUE
-							if($validPassword === true)
-							{
-								//Provide the subscriber with a login session.
-								echo $_SESSION['user_id'] = $row['user_id'];
-								echo $_SESSION['user_email'] = $row['user_email'];
-								echo $_SESSION['role_type'] = $row['role_type'];
-
-								//die();
-
-								$sql = "UPDATE tbl_user
-										SET user_login_time = Now()
-										WHERE user_email = :user_email";
-
-								$stmt = $conn->prepare($sql);
-								$stmt->bindValue(':user_email', $user_email);
-				
-								//Execute.
-								$result = $stmt->execute();
-								
-								if($result > 0)
+								include '../../../../scripts/function.php';
+								if(isset($_POST['login']))
 								{
-									header('Location: dashboard_sale.php');
-									exit;
+									//Retrieve the field values from our login form.
+									$user_email = !empty($_POST['user_email']) ? trim($_POST['user_email']) : null;
+									$passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+									login($user_email, $passwordAttempt);
 								}
-							}
-							else
-							{
-								//Password was FALSE. Passwords do not match.
-								//header("Location: /msgs/msg_invalid_password.php");
-								echo "Invalid Password";
-							}
-
-						}
-					}
-				?>
+							?>
 
                                 <h3 class="font-weight-bolder text-dark font-size-h2 font-size-h1-lg">Sign In</h3>
                                 <div class="text-muted font-weight-bold font-size-h4">New Here?
