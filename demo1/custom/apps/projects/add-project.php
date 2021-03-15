0d87049f292481edf6a2d5c032bdf1da736345d4
Zain Khan <zainkhan8048@gmail.com>
2021-03-15 20:23:17 +0500

    check

--- demo1/custom/apps/projects/add-project.php
+++ demo1/custom/apps/projects/add-project.php
@@ -90,94 +90,20 @@
 															</span>
 															<h3 class="wizard-title">Project Details</h3>
 														</div>
-														<span class="svg-icon svg-icon-xl wizard-arrow">
-															<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
-															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																	<polygon points="0 0 24 0 24 24 0 24" />
-																	<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1" />
-																	<path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)" />
-																</g>
-															</svg>
-															<!--end::Svg Icon-->
-														</span>
+														
 													</div>
+													
 													<div class="wizard-step" data-wizard-type="step">
 														
-														<span class="svg-icon svg-icon-xl wizard-arrow">
-															<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
-															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																	<polygon points="0 0 24 0 24 24 0 24" />
-																	<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1" />
-																	<path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)" />
-																</g>
-															</svg>
-															<!--end::Svg Icon-->
-														</span>
+														
 													</div>
-													<div class="wizard-step" data-wizard-type="step">
-														<div class="wizard-label">
-															<span class="svg-icon svg-icon-4x wizard-icon">
-																<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Home/Globe.svg-->
-																<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																		<rect x="0" y="0" width="24" height="24" />
-																		<path d="M13,18.9450712 L13,20 L14,20 C15.1045695,20 16,20.8954305 16,22 L8,22 C8,20.8954305 8.8954305,20 10,20 L11,20 L11,18.9448245 C9.02872877,18.7261967 7.20827378,17.866394 5.79372555,16.5182701 L4.73856106,17.6741866 C4.36621808,18.0820826 3.73370941,18.110904 3.32581341,17.7385611 C2.9179174,17.3662181 2.88909597,16.7337094 3.26143894,16.3258134 L5.04940685,14.367122 C5.46150313,13.9156769 6.17860937,13.9363085 6.56406875,14.4106998 C7.88623094,16.037907 9.86320756,17 12,17 C15.8659932,17 19,13.8659932 19,10 C19,7.73468744 17.9175842,5.65198725 16.1214335,4.34123851 C15.6753081,4.01567657 15.5775721,3.39010038 15.903134,2.94397499 C16.228696,2.49784959 16.8542722,2.4001136 17.3003976,2.72567554 C19.6071362,4.40902808 21,7.08906798 21,10 C21,14.6325537 17.4999505,18.4476269 13,18.9450712 Z" fill="#000000" fill-rule="nonzero" />
-																		<circle fill="#000000" opacity="0.3" cx="12" cy="10" r="6" />
-																	</g>
-																</svg>
-																<!--end::Svg Icon-->
-															</span>
-															<h3 class="wizard-title">Delivery Details</h3>
-														</div>
-														<span class="svg-icon svg-icon-xl wizard-arrow">
-															<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
-															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																	<polygon points="0 0 24 0 24 24 0 24" />
-																	<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1" />
-																	<path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)" />
-																</g>
-															</svg>
-															<!--end::Svg Icon-->
-														</span>
-													</div>
-													<div class="wizard-step" data-wizard-type="step">
-														<div class="wizard-label">
-															<span class="svg-icon svg-icon-4x wizard-icon">
-																<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/General/Notification2.svg-->
-																<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																		<rect x="0" y="0" width="24" height="24" />
-																		<path d="M13.2070325,4 C13.0721672,4.47683179 13,4.97998812 13,5.5 C13,8.53756612 15.4624339,11 18.5,11 C19.0200119,11 19.5231682,10.9278328 20,10.7929675 L20,17 C20,18.6568542 18.6568542,20 17,20 L7,20 C5.34314575,20 4,18.6568542 4,17 L4,7 C4,5.34314575 5.34314575,4 7,4 L13.2070325,4 Z" fill="#000000" />
-																		<circle fill="#000000" opacity="0.3" cx="18.5" cy="5.5" r="2.5" />
-																	</g>
-																</svg>
-																<!--end::Svg Icon-->
-															</span>
-															<h3 class="wizard-title">Review and Submit</h3>
-														</div>
-														<span class="svg-icon svg-icon-xl wizard-arrow last">
-															<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
-															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																	<polygon points="0 0 24 0 24 24 0 24" />
-																	<rect fill="#000000" opacity="0.3" transform="translate(12.000000, 12.000000) rotate(-90.000000) translate(-12.000000, -12.000000)" x="11" y="5" width="2" height="14" rx="1" />
-																	<path d="M9.70710318,15.7071045 C9.31657888,16.0976288 8.68341391,16.0976288 8.29288961,15.7071045 C7.90236532,15.3165802 7.90236532,14.6834152 8.29288961,14.2928909 L14.2928896,8.29289093 C14.6714686,7.914312 15.281055,7.90106637 15.675721,8.26284357 L21.675721,13.7628436 C22.08284,14.136036 22.1103429,14.7686034 21.7371505,15.1757223 C21.3639581,15.5828413 20.7313908,15.6103443 20.3242718,15.2371519 L15.0300721,10.3841355 L9.70710318,15.7071045 Z" fill="#000000" fill-rule="nonzero" transform="translate(14.999999, 11.999997) scale(1, -1) rotate(90.000000) translate(-14.999999, -11.999997)" />
-																</g>
-															</svg>
-															<!--end::Svg Icon-->
-														</span>
-													</div>
+													
 												</div>
 											</div>
 											<!--end::Wizard Nav-->
 											<!--begin::Wizard Body-->
 											<div class="row justify-content-center my-10 px-8 my-lg-15 px-lg-10">
-												<div class="col-xl-12 col-xxl-7">
-													<!--begin::Form Wizard-->
-													<form class="form" id="kt_projects_add_form">
+											<form class="form" id="kt_projects_add_form">
 														<!--begin::Step 1-->
 														<div class="pb-5" data-wizard-type="step-content" data-wizard-state="current">
 															<h3 class="mb-10 font-weight-bold text-dark">Project Details:</h3>
@@ -242,111 +168,50 @@
 																				<option data-offset="46800" value="Nuku'alofa">Laravel</option>
 																			</select>
 																		</div>
-																		<div class="pb-5" data-wizard-type="step-content">
-															<h4 class="mb-10 font-weight-bold">Review your Details and Submit</h4>
-															
-															<table class="w-100">
-																<tr>
-																	<td class="font-weight-bold text-muted">Name:</td>
-																	<td class="font-weight-bold text-right">Loop Inc CRM App</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Phone:</td>
-																	<td class="font-weight-bold text-right">+61412345678</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Email:</td>
-																	<td class="font-weight-bold text-right">johnwick@reeves.com</td>
-																</tr>
-															</table>
-															<div class="separator separator-dashed my-5"></div>
-															<h6 class="font-weight-bold mb-3">Delivery Info:</h6>
-															<table class="w-100">
-																<tr>
-																	<td class="font-weight-bold text-muted">Address Line 1:</td>
-																	<td class="font-weight-bold text-right">Fox Avenue 5-6B</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Address Line 2:</td>
-																	<td class="font-weight-bold text-right">Melbourne VIC</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Post:</td>
-																	<td class="font-weight-bold text-right">3000</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Country:</td>
-																	<td class="font-weight-bold text-right">Australia</td>
-																</tr>
-															</table>
-															<div class="separator separator-dashed my-5"></div>
-															<h6 class="font-weight-bold mb-3">Payment Details:</h6>
-															<table class="w-100">
-																<tr>
-																	<td class="font-weight-bold text-muted">Card Number:</td>
-																	<td class="font-weight-bold text-right">xxxx xxxx xxxx 1111</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Card Name:</td>
-																	<td class="font-weight-bold text-right">John Wick</td>
-																</tr>
-																<tr>
-																	<td class="font-weight-bold text-muted">Card Expiry:</td>
-																	<td class="font-weight-bold text-right">01/21</td>
-																</tr>
-															</table>
-														</div>	
-																		
 																	</div>
+																	
+																	
+																	<div class="form-group row">
+																	<label class="col-xl-3 col-lg-3 col-form-label">Deadlines</label>
+																	<div class="col-lg-8 col-xl-9">
+																			<input class="form-control form-control-lg form-control-solid" name="deadline" type="date" />
+																		</div>
+																	
 																	<div class="pb-5" data-wizard-type="step-content">
 																	<h3 class="mb-10 font-weight-bold text-dark">Delivery Details</h3>
 																	<div class="form-group row">
 																		<label class="col-xl-3 col-lg-3 col-form-label">Deadlines</label>
 																		<div class="col-lg-9 col-xl-9">
 																			<input class="form-control form-control-lg form-control-solid" name="deadline" type="date" />
+																			<button type="button" class="btn btn-success font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-submit">Submit now</button>
 																		</div>
-																	</div>
+																		
+														</div>
 															
 														</div>
 
 																</div>
 															</div>
 														</div>
-														<!--end::Step 1-->
-														<!--begin::Step 2-->
-														
-														<!--end::Step 2-->
-														<!--begin::Step 3-->
-														<div class="pb-5" data-wizard-type="step-content">
-															<h3 class="mb-10 font-weight-bold text-dark">Delivery Details</h3>
-																	<div class="form-group row">
-																		<label class="col-xl-3 col-lg-3 col-form-label">Deadlines</label>
-																		<div class="col-lg-9 col-xl-9">
-																			<input class="form-control form-control-lg form-control-solid" name="deadline" type="date" />
-																		</div>
-																	</div>
-															<div class="row">
-																<div class="col-xl-6">
-																	
-																</div>
+														<div class="d-flex justify-content-between border-top mt-5 pt-10">
+															<div class="mr-2">
+																<button type="button" class="btn btn-light-primary font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-prev">Previous</button>
+															</div>
+															<div>
+																
+																<button type="button" class="btn btn-primary font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-next">Submit</button>
 															</div>
 														</div>
+													
+														
 														<!--end::Step 3-->
 														<!--begin::Step 4-->
 														
 														<!--end::Step 4-->
 														<!--begin::Actions-->
-														<div class="d-flex justify-content-between border-top mt-5 pt-10">
-															
-															<div>
-																<button type="button" class="btn btn-success font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-submit">Submit</button>
-																
-															</div>
-														</div>
+														
 														<!--end::Actions-->
 													</form>
-													<!--end::Form Wizard-->
-												</div>
 											</div>
 											<!--end::Wizard Body-->
 										</div>
