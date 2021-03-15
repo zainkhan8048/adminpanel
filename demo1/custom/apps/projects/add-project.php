bd38bede3721a68a7d292babc8089ae53a99c7fd
Zain Khan <zainkhan8048@gmail.com>
2021-03-15 19:48:51 +0500

    check

--- demo1/custom/apps/projects/add-project.php
+++ demo1/custom/apps/projects/add-project.php
@@ -103,20 +103,7 @@
 														</span>
 													</div>
 													<div class="wizard-step" data-wizard-type="step">
-														<div class="wizard-label">
-															<span class="svg-icon svg-icon-4x wizard-icon">
-																<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Devices/Display1.svg-->
-																<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-																	<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-																		<rect x="0" y="0" width="24" height="24" />
-																		<path d="M11,20 L11,17 C11,16.4477153 11.4477153,16 12,16 C12.5522847,16 13,16.4477153 13,17 L13,20 L15.5,20 C15.7761424,20 16,20.2238576 16,20.5 C16,20.7761424 15.7761424,21 15.5,21 L8.5,21 C8.22385763,21 8,20.7761424 8,20.5 C8,20.2238576 8.22385763,20 8.5,20 L11,20 Z" fill="#000000" opacity="0.3" />
-																		<path d="M3,5 L21,5 C21.5522847,5 22,5.44771525 22,6 L22,16 C22,16.5522847 21.5522847,17 21,17 L3,17 C2.44771525,17 2,16.5522847 2,16 L2,6 C2,5.44771525 2.44771525,5 3,5 Z M4.5,8 C4.22385763,8 4,8.22385763 4,8.5 C4,8.77614237 4.22385763,9 4.5,9 L13.5,9 C13.7761424,9 14,8.77614237 14,8.5 C14,8.22385763 13.7761424,8 13.5,8 L4.5,8 Z M4.5,10 C4.22385763,10 4,10.2238576 4,10.5 C4,10.7761424 4.22385763,11 4.5,11 L7.5,11 C7.77614237,11 8,10.7761424 8,10.5 C8,10.2238576 7.77614237,10 7.5,10 L4.5,10 Z" fill="#000000" />
-																	</g>
-																</svg>
-																<!--end::Svg Icon-->
-															</span>
-															<h3 class="wizard-title">Project Requirments</h3>
-														</div>
+														
 														<span class="svg-icon svg-icon-xl wizard-arrow">
 															<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Navigation/Arrow-right.svg-->
 															<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -229,22 +216,6 @@
 																		
 																		</div>
 																		
-																		
-																	</div>
-																	
-
-																</div>
-															</div>
-														</div>
-														<!--end::Step 1-->
-														<!--begin::Step 2-->
-														<div class="pb-5" data-wizard-type="step-content">
-															<div class="row">
-																<div class="col-xl-12">
-																	<div class="form-group row">
-																		<div class="col-lg-9 col-xl-6">
-																			<h3 class="mb-10 font-weight-bold text-dark">Project Requirments</h3>
-																		</div>
 																	</div>
 																	<div class="form-group row">
 																		<label class="col-xl-3 col-lg-3 col-form-label">Project Type</label>
@@ -258,11 +229,12 @@
 																				
 																			</select>
 																		</div>
+																		
 																	</div>
 																	<div class="form-group row">
-																		<label class="col-xl-3 col-lg-3 col-form-label">Project Technology</label>
-																		<div class="col-lg-9 col-xl-9">
-																			<select name="timezone" class="form-control form-control-lg form-control-solid">
+																		<label class="col-xl-3 col-lg-3 col-form-label">Technology Type</label>
+																		<div class="col-lg-8 col-xl-9">
+																			<select name="language" class="form-control form-control-lg form-control-solid">
 																				<option>Select Technology...</option>												
 																				<option data-offset="-39600" value="International Date Line West">PHP</option>
 																				<option data-offset="-39600" value="Midway Island">ASP.Net</option>
@@ -270,31 +242,9 @@
 																				<option data-offset="46800" value="Nuku'alofa">Laravel</option>
 																			</select>
 																		</div>
-																	</div>
-																</div>
-															</div>
-														</div>
-														<!--end::Step 2-->
-														<!--begin::Step 3-->
-														<div class="pb-5" data-wizard-type="step-content">
-															<h3 class="mb-10 font-weight-bold text-dark">Delivery Details</h3>
-																	<div class="form-group row">
-																		<label class="col-xl-3 col-lg-3 col-form-label">Deadlines</label>
-																		<div class="col-lg-9 col-xl-9">
-																			<input class="form-control form-control-lg form-control-solid" name="deadline" type="date" />
-																		</div>
-																	</div>
-															<div class="row">
-																<div class="col-xl-6">
-																	
-																</div>
-															</div>
-														</div>
-														<!--end::Step 3-->
-														<!--begin::Step 4-->
-														<div class="pb-5" data-wizard-type="step-content">
+																		<div class="pb-5" data-wizard-type="step-content">
 															<h4 class="mb-10 font-weight-bold">Review your Details and Submit</h4>
-															<h6 class="font-weight-bold mb-3">Project Details:</h6>
+															
 															<table class="w-100">
 																<tr>
 																	<td class="font-weight-bold text-muted">Name:</td>
@@ -345,16 +295,52 @@
 																	<td class="font-weight-bold text-right">01/21</td>
 																</tr>
 															</table>
+														</div>	
+																		
+																	</div>
+																	<div class="pb-5" data-wizard-type="step-content">
+																	<h3 class="mb-10 font-weight-bold text-dark">Delivery Details</h3>
+																	<div class="form-group row">
+																		<label class="col-xl-3 col-lg-3 col-form-label">Deadlines</label>
+																		<div class="col-lg-9 col-xl-9">
+																			<input class="form-control form-control-lg form-control-solid" name="deadline" type="date" />
+																		</div>
+																	</div>
+															
 														</div>
+
+																</div>
+															</div>
+														</div>
+														<!--end::Step 1-->
+														<!--begin::Step 2-->
+														
+														<!--end::Step 2-->
+														<!--begin::Step 3-->
+														<div class="pb-5" data-wizard-type="step-content">
+															<h3 class="mb-10 font-weight-bold text-dark">Delivery Details</h3>
+																	<div class="form-group row">
+																		<label class="col-xl-3 col-lg-3 col-form-label">Deadlines</label>
+																		<div class="col-lg-9 col-xl-9">
+																			<input class="form-control form-control-lg form-control-solid" name="deadline" type="date" />
+																		</div>
+																	</div>
+															<div class="row">
+																<div class="col-xl-6">
+																	
+																</div>
+															</div>
+														</div>
+														<!--end::Step 3-->
+														<!--begin::Step 4-->
+														
 														<!--end::Step 4-->
 														<!--begin::Actions-->
 														<div class="d-flex justify-content-between border-top mt-5 pt-10">
-															<div class="mr-2">
-																<button type="button" class="btn btn-light-primary font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-prev">Previous</button>
-															</div>
+															
 															<div>
 																<button type="button" class="btn btn-success font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-submit">Submit</button>
-																<button type="button" class="btn btn-primary font-weight-bolder text-uppercase px-9 py-4" data-wizard-type="action-next">Next Step</button>
+																
 															</div>
 														</div>
 														<!--end::Actions-->
